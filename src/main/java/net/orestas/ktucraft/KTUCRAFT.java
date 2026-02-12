df039481dc90c6a8ad78d79661b605decf80d027
Orestas <stlemokas32@gmail.com>
2026-02-12 19:13:31 +0200

    Initial commit

---
+++ src/main/java/net/orestas/ktucraft/KTUCRAFT.java
@@ -0,0 +1,16 @@
+package net.orestas.ktucraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KTUCRAFT implements ModInitializer {
+	public static final String MOD_ID = "ktucraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/orestas/ktucraft/KtuCraftClient.java
@@ -0,0 +1,10 @@
+package net.orestas.ktucraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KtuCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
